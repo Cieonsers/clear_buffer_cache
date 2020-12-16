ca3b854a1abbf6d46d20bacf797effec12a34231
DESKTOP-CNU6H97\Raeme <cieonsers@gmail.com>
2020-12-16 09:07:20 +0800

    no message

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-wget --no-check-certificate https://git.io/JLGzO
-cp -rf clear_buffer_cache.sh /home/clear_buffer_cache.sh
+wget -p /home https://git.io/JLGzO --no-check-certificate
 chmod +x /home/clear_buffer_cache.sh
 
 crontab -l > crontab_clearbc
-echo "* /5 * * * bash /home/clear_buffer_cache.sh" >> crontab_clearbc
+echo "* */5 * * * bash /home/clear_buffer_cache.sh" >> crontab_clearbc
 crontab crontab_clearbc
 rm -rf crontab_clearbc
-rm -rf clear_buffer_cache.sh
