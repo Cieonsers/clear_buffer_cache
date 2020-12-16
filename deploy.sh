97b824ba914f5cbbb619239e62c15693b41fc458
Cieonsers <41176658+Cieonsers@users.noreply.github.com>
2020-12-16 17:24:11 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ cp -rf clear_buffer_cache.sh /home/clear_buffer_cache.sh
 chmod +x /home/clear_buffer_cache.sh
 
 crontab -l > crontab_clearbc
-echo "* */5 * * * /home/clear_buffer_cache.sh" >> crontab_clearbc
+echo "* */5 * * * bash /home/clear_buffer_cache.sh" >> crontab_clearbc
 crontab crontab_clearbc
 rm -rf crontab_clearbc
 rm -rf clear_buffer_cache.sh
